ace92b7ccdec2f2dee7cc467da3fca6c647f2235
HenryRiddall <henryriddall@gmail.com>
2021-01-25 16:41:20 +0000

    Hello World

---
+++ Project/src/main/java/hlr1g18/App.java
@@ -0,0 +1,13 @@
+package hlr1g18;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
